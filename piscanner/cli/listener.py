ed91936b8b935cfa063f14664c81df4bede53e6b
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2025-05-22 13:28:18 +0200

    add codes

--- piscanner/cli/listener.py
+++ piscanner/cli/listener.py
@@ -1,14 +1,19 @@
 import asyncio, evdev, click
 
+from evdev.ecodes import ecodes
+
 async def print_events(device):
     async for event in device.async_read_loop():
         print(device.path, evdev.categorize(event), sep=': ')
 
+def codes():
+    for c in '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+        yield c, ecodes['KEY_{}'.format(c)]
 
 @click.command(help="Listen for barcode scanner")
 def listen():
 
-    devices =[evdev.InputDevice(path) for path in evdev.list_devices()]
+    devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
 
     assert len(devices) > 0, "No devices found"
 
