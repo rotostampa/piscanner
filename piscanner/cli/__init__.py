bfcf89f1c99c4de14c0bb10d037c68a56a22b744
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2025-05-26 12:27:57 +0200

    test all lights

--- piscanner/cli/__init__.py
+++ piscanner/cli/__init__.py
@@ -13,9 +13,7 @@ for module, cmd in (
     ("piscanner.cli.start", "start"),
     ("piscanner.cli.noop", "noop"),
     ("piscanner.cli.populate", "populate"),
-    ("piscanner.cli.lights", "cleanup"),
-    ("piscanner.cli.lights", "alert"),
-    ("piscanner.cli.lights", "success"),
+    ("piscanner.cli.lights", "lights"),
     ("piscanner.cli.populate", "cleanup"),
 ):
     cli.add_command(getattr(import_module(module), cmd))
--- piscanner/cli/lights.py
+++ piscanner/cli/lights.py
@@ -2,34 +2,19 @@ import asyncio
 import click
 import time
 
-from piscanner.utils.lights import setup_gpio, cleanup_gpio, flash_green, flash_red
+from piscanner.utils.lights import setup_gpio, cleanup_gpio, flash_green, flash_red, flash_yellow
 
 
-@click.command(help="Test wait")
-@click.option("--duration", default="1", type=int, help="Time to wait for")
-@click.option("--wait", default="1", type=int, help="Time to wait for")
-def alert(duration, wait):
 
-    setup_gpio()
-    asyncio.run(flash_red(duration=duration, wait=wait, verbose=True))
-    cleanup_gpio()
-
+async def test_lights():
+    for func in (flash_red, flash_green, flash_yellow):
+        await func(duration=1, wait=1, verbose=True)
 
 @click.command(help="Test wait")
 @click.option("--duration", default="1", type=int, help="Time to wait for")
 @click.option("--wait", default="1", type=int, help="Time to wait for")
-def success(duration, wait):
+def lights(duration, wait):
 
     setup_gpio()
-    asyncio.run(flash_green(duration=duration, wait=wait, verbose=True))
-    cleanup_gpio()
-
-
-@click.command(help="Test wait")
-def cleanup():
-
-    print("setup")
-    setup_gpio()
-    time.sleep(1)
-    print("cleanup")
+    asyncio.run(test_lights())
     cleanup_gpio()
