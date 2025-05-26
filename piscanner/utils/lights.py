d5befdb663309fb50f628c64d9cef6f00f817cd4
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2025-05-26 09:34:00 +0200

    add debug code

--- piscanner/utils/lights.py
+++ piscanner/utils/lights.py
@@ -1,6 +1,8 @@
 import asyncio
 from functools import partial
 
+from piscanner.utils.machine import is_mac
+
 lights_lock = asyncio.Lock()
 
 # Define pins
@@ -9,6 +11,10 @@ GREEN_PIN = 3
 
 
 def setup_gpio():
+
+    if is_mac:
+        return
+
     import RPi.GPIO as GPIO
 
     GPIO.setmode(GPIO.BCM)
@@ -19,6 +25,10 @@ def setup_gpio():
 
 
 def cleanup_gpio():
+
+    if is_mac:
+        return
+
     import RPi.GPIO as GPIO
 
     GPIO.cleanup()
@@ -26,16 +36,20 @@ def cleanup_gpio():
 
 # The generic async function
 async def control_light(duration: float, wait: float, pin: int):
-    import RPi.GPIO as GPIO
+
+    if not is_mac:
+        import RPi.GPIO as GPIO
 
     async with lights_lock:
 
         print(f"Turning ON light on GPIO{pin}")
-        GPIO.output(pin, GPIO.LOW)
+        if not is_mac:
+            GPIO.output(pin, GPIO.LOW)
         await asyncio.sleep(duration)
 
         print(f"Turning OFF light on GPIO{pin}")
-        GPIO.output(pin, GPIO.HIGH)
+        if not is_mac:
+            GPIO.output(pin, GPIO.HIGH)
         await asyncio.sleep(wait)
 
 
