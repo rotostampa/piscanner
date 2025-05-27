cddd59db6bd6b21f19a2d4f0924a00a0f9ec0212
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2025-05-27 17:24:32 +0200

    use 001

--- piscanner/utils/storage.py
+++ piscanner/utils/storage.py
@@ -9,7 +9,7 @@ from piscanner.utils.datastructures import data
 from contextlib import asynccontextmanager
 
 
-DB_FILE = "piscanner-v7.db"
+DB_FILE = "piscanner-001.db"
 
 if is_mac:
     DB_FILE = os.path.join(os.path.dirname(__file__), DB_FILE)
