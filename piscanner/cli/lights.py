d0e9764c696a77ad9ae2a4c9d32b291f956c406a
Riccardo Di Virgilio <riccardodivirgilio@gmail.com>
2025-05-27 10:53:18 +0200

    Update lights.py

--- piscanner/cli/lights.py
+++ piscanner/cli/lights.py
@@ -5,7 +5,7 @@ from piscanner.utils.lights import setup_gpio, cleanup_gpio, flash_green, flash_
 
 
 async def test_lights():
-    for i in range(10):
+    while True:
         for func in (flash_red, flash_green, flash_yellow):
             await func(duration=0.2, wait=0.1, verbose=True)
 
